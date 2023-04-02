0cab5bcc5d6103b3f31cc5615bc9115f4d26a55b
Ravi Kumar <79511972+raviKumar-bsgi@users.noreply.github.com>
2023-04-02 13:23:23 +0530

    OpenCSV dependencies added POJO updated with needed openCSV annotn

--- src/main/java/com/springrestmvcproject/spring6restmvc/model/BeerCSVRecord.java
+++ src/main/java/com/springrestmvcproject/spring6restmvc/model/BeerCSVRecord.java
@@ -1,5 +1,6 @@
 package com.springrestmvcproject.spring6restmvc.model;
 
+import com.opencsv.bean.CsvBindByName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,19 +10,34 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class BeerCSVRecord {
+
+    @CsvBindByName
     private Integer row;
+    @CsvBindByName(column = "column.x")
     private Integer count;
+    @CsvBindByName
     private String abv;
+    @CsvBindByName
     private String ibu;
+    @CsvBindByName
     private Integer id;
+    @CsvBindByName
     private String beer;
+    @CsvBindByName
     private String style;
+    @CsvBindByName(column = "brewery_id")
     private Integer breweryId;
+    @CsvBindByName
     private Float ounces;
+    @CsvBindByName
     private String style2;
+    @CsvBindByName(column = "count.y")
     private String count_y;
+    @CsvBindByName
     private String city;
+    @CsvBindByName
     private String state;
+    @CsvBindByName
     private String label;
 
 }
